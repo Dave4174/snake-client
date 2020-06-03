32b54d4398dd8713c76226e1749bed08af3f32b1
Dave4174 <dlang4174@gmail.com>
2020-06-03 20:15:29 +0000

    WASD Movement

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
+let connection;
+
 // Setup User Interface 
 // Specifically, so that we can handle user input via stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -17,6 +20,14 @@ const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    conn.write('Move: up');
+  } else if (key === 'a') {
+    conn.write('Move: left');
+  } else if (key === 's') {
+    conn.write('Move: down');
+  } else if (key === 'd') {
+    conn.write('Move: right');
   }
 }
 
